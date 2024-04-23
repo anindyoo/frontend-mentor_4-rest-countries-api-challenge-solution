26b69992740e613a25cdd81ce84102925219c0fc
anindyoo <anindyo0216@gmail.com>
2024-04-23 20:28:11 +0700

    feat: add app routings.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import CountryDetail from './routes/CountryDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path=":id" element={<CountryDetail />} />
+        </Route>
+      </Routes> 
+    </BrowserRouter>
   </React.StrictMode>
 );
