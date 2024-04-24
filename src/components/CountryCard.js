7b207c14dc6a08db6f6cf6a83c2f1ddc0a17ffe7
anindyoo <anindyo0216@gmail.com>
2024-04-24 17:13:11 +0700

    feat: add static card component.

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,8 +1,32 @@
 const CountryCard = () => {
   return (
-    <div>
-      
-    </div>
+    <li className="
+      rounded-md
+      bg-white dark:bg-darkBlue
+      overflow-hidden
+      drop-shadow-gray dark:drop-shadow-darkGray">
+      <img src="https://dummyimage.com/264x160/000/fff" alt="" />
+      <section className="flex flex-col p-6 pb-11 gap-y-4 max-h-[11rem]">
+        <h1 className="text-lg font-bold">Germany</h1>
+        <div className="
+          flex flex-col gap-y-[0.313rem]
+          text-sm font-semibold"
+        >
+          <h2>
+            <span>Population: </span>
+            <span className="font-light">81,772,000</span>
+          </h2>
+          <h2>
+            <span>Region: </span>
+            <span className="font-light">Europe</span>
+          </h2>
+          <h2>
+            <span>Capital: </span>
+            <span className="font-light">Berlin</span>
+          </h2>
+        </div>
+      </section>
+    </li>
   )
 }
 
