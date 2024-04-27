93a470c768b40195bb283567a0851a9c3c84d8da
anindyoo <anindyo0216@gmail.com>
2024-04-27 22:59:59 +0700

    feat: wrap country card with Link tag to detail page.

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const CountryCard = (props) => {
   const {
     name,
@@ -6,46 +8,51 @@ const CountryCard = (props) => {
     capital,
     flag,
     flagAlt,
+    cca3,
   } = props;
 
   const formattedPopulation = population.toLocaleString('en-US');
-  const formattedCapital = capital && capital.join(', ');
+  const formattedCapital = capital.length ? capital.join(', ') : '-';
 
   return (
-    <li className="
-      rounded-md
-      transition-colors duration-75 ease-in-out
-      bg-white dark:bg-darkBlue
-      overflow-hidden
-      drop-shadow-gray dark:drop-shadow-darkGray">
-      <img 
-        src={flag} 
-        alt={flagAlt ? flagAlt : `Flag of ${name}`} 
-        className="
-          w-[16.5rem] h-[10rem]
-          object-cover"          
-      />
-      <section className="flex flex-col p-6 pb-11 gap-y-4 max-h-[11rem]">
-        <h1 className="text-lg font-bold">{name}</h1>
-        <div className="
-          flex flex-col gap-y-[0.313rem]
-          text-sm font-semibold"
-        >
-          <h2>
-            <span>Population: </span>
-            <span className="font-light">{formattedPopulation}</span>
-          </h2>
-          <h2>
-            <span>Region: </span>
-            <span className="font-light">{region}</span>
-          </h2>
-          <h2>
-            <span>Capital: </span>
-            <span className="font-light">{formattedCapital}</span>
-          </h2>
-        </div>
-      </section>
-    </li>
+    <Link
+      to={`/${cca3}`}  
+    >
+      <li className="
+        rounded-md
+        transition-colors duration-75 ease-in-out
+        bg-white dark:bg-darkBlue
+        overflow-hidden
+        drop-shadow-gray dark:drop-shadow-darkGray">
+        <img 
+          src={flag} 
+          alt={flagAlt ? flagAlt : `Flag of ${name}`} 
+          className="
+            w-[16.5rem] h-[10rem]
+            object-cover"          
+        />
+        <section className="flex flex-col p-6 pb-11 gap-y-4 max-h-[11rem]">
+          <h1 className="text-lg font-bold">{name}</h1>
+          <div className="
+            flex flex-col gap-y-[0.313rem]
+            text-sm font-semibold"
+          >
+            <h2>
+              <span>Population: </span>
+              <span className="font-light">{formattedPopulation}</span>
+            </h2>
+            <h2>
+              <span>Region: </span>
+              <span className="font-light">{region}</span>
+            </h2>
+            <h2>
+              <span>Capital: </span>
+              <span className="font-light">{formattedCapital}</span>
+            </h2>
+          </div>
+        </section>
+      </li>
+    </Link>
   )
 }
 
