d1bc654f85a47ce73181fa05ae814979bf2cfdc0
anindyoo <anindyo0216@gmail.com>
2024-04-27 18:45:43 +0700

    feat: create back button component.

---
+++ src/components/BackButton.js
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom"
+
+const BackButton = () => {
+  const navigate = useNavigate();
+  
+  return (
+    <button
+      type="button" 
+      onClick={() => navigate(-1)}
+      className="
+        flex flex-row justify-center items-center gap-3
+        py-[0.563rem]
+        max-w-[8.5rem] w-full rounded-[0.125rem]
+        bg-white dark:bg-darkBlue
+        drop-shadow-thickGray dark:drop-shadow-thickDarkGray"
+    >
+      <svg 
+        width="18" 
+        height="12" 
+        viewBox="0 0 18 12" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path 
+          d="M17 6H1M1 6L6 1M1 6L6 11" 
+          stroke="currentColor" 
+          strokeWidth="1.8" 
+          strokeLinecap="round" 
+          strokeLinejoin="round"
+        />
+      </svg>
+      <span>Back</span>
+    </button> 
+  );
+}
+
+export default BackButton
