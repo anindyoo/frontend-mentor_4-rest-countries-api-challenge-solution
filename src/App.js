6339207bcf014b7e4024add1339a3f9d8a0e4154
anindyoo <anindyo0216@gmail.com>
2024-04-28 10:13:42 +0700

    feat: update components to return routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { createContext, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Main from "./layout/Main";
+import Home from "./routes/Home";
+import CountryDetail from "./routes/CountryDetail";
 
 export const DarkModeContext = createContext();
 export const API_BASE_URL = 'https://restcountries.com/v3.1';
@@ -7,56 +10,17 @@ export const API_SEARCH_BY_NAME_URL = 'https://restcountries.com/v3.1/name'
 export const API_SEARCH_BY_CODE_URL = 'https://restcountries.com/v3.1/alpha'
 export const API_FIELDS_PARAM = 'fields=name,population,region,capital,flags,cca3';
 
-function App() {
+const App = () => {
   const [darkModeState, setDarkModeState] = useState(false);
   
-  const toggleDarkMode = () => {
-    setDarkModeState(!darkModeState);
-    document.body.classList.toggle("dark");
-  };
-
   return (
-    <DarkModeContext.Provider value={{ darkModeState }}>
-      <div className={`
-        APP
-        h-screen max-w-[90rem] w-screen
-        text-veryDarkBlue_LightModeText dark:text-white
-        px-4      
-        lg:px-12
-        xl:px-20
-        `}>
-        <header className="
-          flex flex-row justify-between
-          py-6"
-        >
-          <Link to={`/`}>
-            <h1 className="text-2xl font-extrabold">Where in the world?</h1>
-          </Link>
-          <button 
-            className="flex flex-row gap-2.5 items-center"
-            onClick={toggleDarkMode}>
-            <svg 
-              width="17" 
-              height="17" 
-              viewBox="0 0 17 17" 
-              fill={darkModeState ? `currentColor` : `none`} 
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path 
-                d="M16 11.2021C15.0491 11.5984 14.0289 11.8018 12.9987 11.8004C8.69118 11.8004 5.19955 8.30882 5.19955 4.00128C5.19955 2.93739 5.41233 1.9239 5.79789 1C4.37681 1.59283 3.16294 2.59291 2.30916 3.8743C1.45538 5.15568 0.999868 6.66106 1 8.20083C1 12.5084 4.49163 16 8.79917 16C10.3389 16.0001 11.8443 15.5446 13.1257 14.6908C14.4071 13.8371 15.4072 12.6232 16 11.2021Z" 
-                stroke="currentColor" 
-                strokeWidth="1.5" 
-                strokeLinecap="round" 
-                strokeLinejoin="round"
-              />
-            </svg>
-            <p>Dark Mode</p>
-          </button>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
+    <DarkModeContext.Provider value={{ darkModeState, setDarkModeState }}>
+      <Routes>
+        <Route path="/" element={<Main />} exact>
+          <Route index element={<Home />} />
+          <Route path=":id" element={<CountryDetail />} exact />
+        </Route>
+      </Routes> 
     </DarkModeContext.Provider>
   );
 }
