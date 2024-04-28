17ba78700eef13b95307a3d143a2e3e35d0ab6fc
anindyoo <anindyo0216@gmail.com>
2024-04-28 10:12:25 +0700

    feat: create separate layout for main app content.

---
+++ src/layout/Main.js
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const Main = () => {
+
+  return ( 
+    <div className="
+      MAIN-APP
+      h-screen w-screen
+      text-veryDarkBlue_LightModeText dark:text-white"
+    >
+      <Navbar />
+      <main className="
+        MAIN-CONTENT
+        flex justify-center
+        w-screen
+        md:px-10        
+        lg:px-12
+        xl:px-20"
+      >
+        <Outlet />
+      </main>
+    </div>   
+  )
+}
+
+export default Main;
