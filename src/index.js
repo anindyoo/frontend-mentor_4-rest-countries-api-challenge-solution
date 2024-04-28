e06ff6cd67f4fc501d24fa2c33536c18f5aa1015
anindyoo <anindyo0216@gmail.com>
2024-04-28 10:13:10 +0700

    feat: update browser router child component.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './routes/Home';
-import CountryDetail from './routes/CountryDetail';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} exact>
-          <Route index element={<Home />} />
-          <Route path=":id" element={<CountryDetail />} exact/>
-        </Route>
-      </Routes> 
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
